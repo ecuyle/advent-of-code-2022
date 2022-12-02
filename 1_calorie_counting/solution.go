36a11884fb32e69f6e6ed8717e846a37b9843870
Eric Cuyle <ecuyle@gmail.com>
2022-12-02 02:46:36 -0800

    Refactors solution to day 1 to share a single util to calculate answers for part 1 and 2

--- 1_calorie_counting/solution.go
+++ 1_calorie_counting/solution.go
@@ -1,83 +1,57 @@
 package main
 
 import (
-  "bufio"
-  "fmt"
-  "os"
-  "sort"
-  "strconv"
+    "bufio"
+    "fmt"
+    "os"
+    "sort"
+    "strconv"
 )
 
 func check(e error) {
-  if e != nil {
-    panic(e)
-  }
+    if e != nil {
+        panic(e)
+    }
+}
+
+func calculateTotalCalories(places int, filepath string) int {
+    file, err := os.Open(filepath)
+    check(err)
+    defer file.Close()
+
+    scanner := bufio.NewScanner(file)
+    caloricHistory := []int{0}
+    calories := 0
+
+    for scanner.Scan() {
+        raw := scanner.Text()
+
+        if raw != "" {
+            value, err := strconv.Atoi(raw)
+            check(err)
+            calories += value
+            continue
+        }
+
+        caloricHistory = append(caloricHistory, calories) 
+        calories = 0
+    }
+
+    sort.Ints(caloricHistory)
+    totalCalories := 0
+
+    for i := 0; i < places; i++ {
+        if len(caloricHistory) <= i {
+            break
+        }
+
+        totalCalories += caloricHistory[len(caloricHistory) - i - 1]
+    }
+
+    return totalCalories
 }
 
 func main() {
-  file, err := os.Open("./input.txt")
-  check(err)
-  defer file.Close()
-
-  scanner := bufio.NewScanner(file)
-  maxCalories := 0
-  calories := 0
-
-  for scanner.Scan() {
-    raw := scanner.Text()
-
-    if raw != "" {
-      value, err := strconv.Atoi(raw)
-      check(err)
-      calories += value
-      continue
-    }
-
-    if calories > maxCalories {
-      maxCalories = calories
-    }
-
-    calories = 0
-  }
-
-  if calories > maxCalories {
-    maxCalories = calories
-  }
-
-  fmt.Println("Max calories carried by an elf is", maxCalories)
-
-  file, err = os.Open("./input.txt")
-  check(err)
-  defer file.Close()
-
-  scanner = bufio.NewScanner(file)
-  caloricHistory := []int{0}
-  calories = 0
-
-  for scanner.Scan() {
-    raw := scanner.Text()
-
-    if raw != "" {
-      value, err := strconv.Atoi(raw)
-      check(err)
-      calories += value
-      continue
-    }
-
-    caloricHistory = append(caloricHistory, calories) 
-    calories = 0
-  }
-
-  sort.Ints(caloricHistory)
-  totalCalories := 0
-
-  for i := 0; i < 3; i++ {
-    if len(caloricHistory) <= i {
-      break
-    }
-
-    totalCalories += caloricHistory[len(caloricHistory) - i - 1]
-  }
-
-  fmt.Println("Total calories are:", totalCalories)
+    fmt.Println("Max calories carried by an elf is", calculateTotalCalories(1, "./input.txt"))
+    fmt.Println("Total calories of top three elves are:", calculateTotalCalories(3, "./input.txt"))
 }
