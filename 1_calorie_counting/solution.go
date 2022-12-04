ccbd81309821c5f3025887eea7245cf43cab4dfb
Eric Cuyle <ecuyle@gmail.com>
2022-12-03 23:00:27 -0800

    Testing out gofmt

--- 1_calorie_counting/solution.go
+++ 1_calorie_counting/solution.go
@@ -1,57 +1,57 @@
 package main
 
 import (
-    "bufio"
-    "fmt"
-    "os"
-    "sort"
-    "strconv"
+	"bufio"
+	"fmt"
+	"os"
+	"sort"
+	"strconv"
 )
 
 func check(e error) {
-    if e != nil {
-        panic(e)
-    }
+	if e != nil {
+		panic(e)
+	}
 }
 
 func calculateTotalCalories(places int, filepath string) int {
-    file, err := os.Open(filepath)
-    check(err)
-    defer file.Close()
+	file, err := os.Open(filepath)
+	check(err)
+	defer file.Close()
 
-    scanner := bufio.NewScanner(file)
-    caloricHistory := []int{0}
-    calories := 0
+	scanner := bufio.NewScanner(file)
+	caloricHistory := []int{0}
+	calories := 0
 
-    for scanner.Scan() {
-        raw := scanner.Text()
+	for scanner.Scan() {
+		raw := scanner.Text()
 
-        if raw != "" {
-            value, err := strconv.Atoi(raw)
-            check(err)
-            calories += value
-            continue
-        }
+		if raw != "" {
+			value, err := strconv.Atoi(raw)
+			check(err)
+			calories += value
+			continue
+		}
 
-        caloricHistory = append(caloricHistory, calories) 
-        calories = 0
-    }
+		caloricHistory = append(caloricHistory, calories)
+		calories = 0
+	}
 
-    sort.Ints(caloricHistory)
-    totalCalories := 0
+	sort.Ints(caloricHistory)
+	totalCalories := 0
 
-    for i := 0; i < places; i++ {
-        if len(caloricHistory) <= i {
-            break
-        }
+	for i := 0; i < places; i++ {
+		if len(caloricHistory) <= i {
+			break
+		}
 
-        totalCalories += caloricHistory[len(caloricHistory) - i - 1]
-    }
+		totalCalories += caloricHistory[len(caloricHistory)-i-1]
+	}
 
-    return totalCalories
+	return totalCalories
 }
 
 func main() {
-    fmt.Println("Max calories carried by an elf is", calculateTotalCalories(1, "./input.txt"))
-    fmt.Println("Total calories of top three elves are:", calculateTotalCalories(3, "./input.txt"))
+	fmt.Println("Max calories carried by an elf is", calculateTotalCalories(1, "./input.txt"))
+	fmt.Println("Total calories of top three elves are:", calculateTotalCalories(3, "./input.txt"))
 }
