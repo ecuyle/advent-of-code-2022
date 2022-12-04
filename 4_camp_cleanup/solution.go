b841028cced8735deba8a904f1a7dd0079d5fd50
Eric Cuyle <ecuyle@gmail.com>
2022-12-04 14:33:10 -0800

    Implements solution to day 4 part 2

--- 4_camp_cleanup/solution.go
+++ 4_camp_cleanup/solution.go
@@ -25,7 +25,7 @@ func splitStringPairIntoIntPair(str string, delimiter string) []int {
 	return []int{atoi(pair[0]), atoi(pair[1])}
 }
 
-func main() {
+func partOne() {
 	file := utils.Readfile("./input.txt")
 	defer file.Close()
 	scanner := bufio.NewScanner(file)
@@ -49,10 +49,39 @@ func main() {
 		}
 
 		if firstPair[1] > secondPair[1] {
-			fmt.Println(firstPair, secondPair)
 			result += 1
 		}
 	}
 
 	fmt.Println(result)
+}
+
+func partTwo() {
+	file := utils.Readfile("./input.txt")
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	overlaps := 0
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		assignments := strings.Split(line, ",")
+		firstPair := splitStringPairIntoIntPair(assignments[0], "-")
+		secondPair := splitStringPairIntoIntPair(assignments[1], "-")
+
+		// normalize such that firstPair starts earlier than second
+		if secondPair[0] < firstPair[0] {
+			firstPair, secondPair = secondPair, firstPair
+		}
+
+		if (firstPair[0] <= secondPair[0] && firstPair[1] >= secondPair[0]) || (firstPair[1] >= secondPair[0] && firstPair[1] <= secondPair[1]) {
+			overlaps += 1
+		}
+	}
+
+	fmt.Println(overlaps)
+}
+
+func main() {
+	partOne()
+	partTwo()
 }
