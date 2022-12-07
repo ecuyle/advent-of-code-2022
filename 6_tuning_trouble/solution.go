f3c5bbc8be9d023244a9a674bb17754501390c16
Eric Cuyle <ecuyle@gmail.com>
2022-12-07 00:41:35 -0800

    Optimizes solution to day 6 parts 1 and 2

--- 6_tuning_trouble/solution.go
+++ 6_tuning_trouble/solution.go
@@ -50,7 +50,45 @@ func findSignalLockMarker(inputPath string, signalLockLength int) int {
 	return -1
 }
 
+func findSignalLockMarkerOptimized(inputPath string, signalLockLength int) int {
+	file := utils.Readfile(inputPath)
+	defer file.Close()
+	reader := bufio.NewReader(file)
+	seen := map[string]int{}
+	p1 := 0
+	p2 := 0
+	successes := 0
+
+	for {
+		rune, _, err := reader.ReadRune()
+
+		if err != nil {
+			break
+		}
+
+		char := string(rune)
+
+		if index, ok := seen[char]; ok && index >= p1 {
+			p1 = index + 1
+			successes = p2 - p1
+		}
+
+		successes += 1
+		seen[char] = p2
+		p2 += 1
+
+		if successes == signalLockLength {
+			return p2
+		}
+	}
+
+	return -1
+}
+
 func main() {
 	fmt.Println("Part 1 answer is:", findSignalLockMarker("./input.txt", 4))
 	fmt.Println("Part 2 answer is:", findSignalLockMarker("./input.txt", 14))
+
+	fmt.Println("Part 1 answer is:", findSignalLockMarkerOptimized("./input.txt", 4))
+	fmt.Println("Part 2 answer is:", findSignalLockMarkerOptimized("./input.txt", 14))
 }
