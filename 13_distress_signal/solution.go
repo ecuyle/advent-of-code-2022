534c46f7885974203b2ce1a5a6a3c88cf6bd0990
Eric <ecuyle@gmail.com>
2022-12-29 02:46:07 -0800

    Implements working solution (with debug statements) to day 13 part 1. Will clean up later

--- 13_distress_signal/solution.go
+++ 13_distress_signal/solution.go
@@ -1,36 +1,98 @@
 package main
 
 import (
-	"bufio"
+	"encoding/json"
 	"fmt"
+	"strings"
 
 	"github.com/ecuyle/advent-of-code-2022/utils"
 )
 
-func partOne(inputPath string) {
-	file := utils.Readfile(inputPath)
-	defer file.Close()
-	scanner := bufio.NewScanner(file)
+type Packet []interface{}
 
-	for scanner.Scan() {
-		line := scanner.Text()
-		fmt.Println(line)
+func isInRightOrder(left, right Packet) int {
+	fmt.Println("Comparing", left, right)
+	for i := 0; i < len(left); i++ {
+		if i >= len(right) {
+			return -1
+		}
+
+		lFloat, isLeftFloat := left[i].(float64)
+		rFloat, isRightFloat := right[i].(float64)
+		lArray, isLeftArray := left[i].([]interface{})
+		rArray, isRightArray := right[i].([]interface{})
+
+		fmt.Println(lFloat, rFloat)
+
+		if isLeftArray && isRightArray {
+			res := isInRightOrder(lArray, rArray)
+			if res != 0 {
+				return res
+			}
+		}
+
+		if isLeftFloat && isRightArray {
+			res := isInRightOrder(append(Packet{}, lFloat), rArray)
+			if res != 0 {
+				return res
+			}
+		}
+
+		if isRightFloat && isLeftArray {
+			res := isInRightOrder(lArray, append(Packet{}, rFloat))
+			if res != 0 {
+				return res
+			}
+		}
+
+		if lFloat < rFloat {
+			return 1
+		} else if lFloat > rFloat {
+			return -1
+		}
 	}
 
+	if len(right) > len(left) {
+		return 1
+	}
+
+	return 0
 }
 
-func partTwo(inputPath string) {
-	file := utils.Readfile(inputPath)
-	defer file.Close()
-	scanner := bufio.NewScanner(file)
+func parseLine(line []byte) Packet {
+	packet := Packet{}
+	json.Unmarshal(line, &packet)
 
-	for scanner.Scan() {
-		line := scanner.Text()
-		fmt.Println(line)
+	return packet
+}
+
+func partOne(lines []string) int {
+	sum := 0
+	current := 1
+
+	for len(lines) > 0 {
+		pair := strings.Split(lines[0], "\n")
+		left := parseLine([]byte(pair[0]))
+		right := parseLine([]byte(pair[1]))
+		lines = lines[1:]
+
+		res := isInRightOrder(left, right)
+		fmt.Println("After comparing:", res)
+		fmt.Println(left, right)
+		if res == 1 {
+			fmt.Println("Adding current:", current)
+			sum += current
+		}
+		fmt.Println("-----------------------------------------------------------------------")
+		current += 1
 	}
+
+	return sum
 }
 
 func main() {
-	partOne("./input_test.txt")
-	// partTwo("./input_test.txt")
+	// str := utils.ReadFileIntoString("./input_test.txt")
+	str := utils.ReadFileIntoString("./input.txt")
+	lines := strings.Split(str, "\n\n")
+	fmt.Println(partOne(lines))
 }
