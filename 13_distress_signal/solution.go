8681dc75a4d39124400ffe5a3f3f3eaecee13a41
Eric <ecuyle@gmail.com>
2023-01-03 11:09:57 -0800

    Implements solution to day 13 part 2 and refactors day 13 part 1

--- 13_distress_signal/solution.go
+++ 13_distress_signal/solution.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"sort"
 	"strings"
 
 	"github.com/ecuyle/advent-of-code-2022/utils"
@@ -11,7 +12,6 @@ import (
 type Packet []interface{}
 
 func isInRightOrder(left, right Packet) int {
-	fmt.Println("Comparing", left, right)
 	for i := 0; i < len(left); i++ {
 		if i >= len(right) {
 			return -1
@@ -22,30 +22,23 @@ func isInRightOrder(left, right Packet) int {
 		lArray, isLeftArray := left[i].([]interface{})
 		rArray, isRightArray := right[i].([]interface{})
 
-		fmt.Println(lFloat, rFloat)
-
 		if isLeftArray && isRightArray {
 			res := isInRightOrder(lArray, rArray)
 			if res != 0 {
 				return res
 			}
-		}
-
-		if isLeftFloat && isRightArray {
+		} else if isLeftFloat && isRightArray {
 			res := isInRightOrder(append(Packet{}, lFloat), rArray)
 			if res != 0 {
 				return res
 			}
-		}
-
-		if isRightFloat && isLeftArray {
+		} else if isRightFloat && isLeftArray {
 			res := isInRightOrder(lArray, append(Packet{}, rFloat))
 			if res != 0 {
 				return res
 			}
-		}
-
-		if lFloat < rFloat {
+			continue
+		} else if lFloat < rFloat {
 			return 1
 		} else if lFloat > rFloat {
 			return -1
@@ -66,33 +59,77 @@ func parseLine(line []byte) Packet {
 	return packet
 }
 
-func partOne(lines []string) int {
+func partOne(packets []Packet) int {
 	sum := 0
 	current := 1
 
-	for len(lines) > 0 {
-		pair := strings.Split(lines[0], "\n")
-		left := parseLine([]byte(pair[0]))
-		right := parseLine([]byte(pair[1]))
-		lines = lines[1:]
+	for len(packets) > 0 {
+		left := packets[0]
+		right := packets[1]
+		packets = packets[2:]
 
 		res := isInRightOrder(left, right)
-		fmt.Println("After comparing:", res)
-		fmt.Println(left, right)
+
 		if res == 1 {
-			fmt.Println("Adding current:", current)
 			sum += current
 		}
-		fmt.Println("-----------------------------------------------------------------------")
+
 		current += 1
 	}
 
 	return sum
 }
 
+func isDivider(packet Packet) bool {
+	if len(packet) != 1 {
+		return false
+	}
+
+	arr, isArray := packet[0].([]interface{})
+
+	if !isArray || len(arr) != 1 {
+		return false
+	}
+
+	value, isFloat := arr[0].(float64)
+
+	return isFloat && (value == 2 || value == 6)
+}
+
+func partTwo(packets []Packet) int {
+	dividers := []Packet{parseLine([]byte("[[2]]")), parseLine([]byte("[[6]]"))}
+	packets = append(packets, dividers...)
+	product := 1
+	sort.Slice(packets, func(i, j int) bool {
+		return isInRightOrder(packets[i], packets[j]) == 1
+	})
+
+	for i, packet := range packets {
+		if isDivider(packet) {
+			product *= (i + 1)
+		}
+	}
+
+	return product
+}
+
+func getPackets(input string) []Packet {
+	strs := strings.Split(input, "\n\n")
+	packets := []Packet{}
+
+	for _, str := range strs {
+		rawPair := strings.Split(str, "\n")
+		left := parseLine([]byte(rawPair[0]))
+		right := parseLine([]byte(rawPair[1]))
+		packets = append(packets, left, right)
+	}
+
+	return packets
+}
+
 func main() {
 	// str := utils.ReadFileIntoString("./input_test.txt")
 	str := utils.ReadFileIntoString("./input.txt")
-	lines := strings.Split(str, "\n\n")
-	fmt.Println(partOne(lines))
+	fmt.Println(partOne(getPackets(str)))
+	fmt.Println(partTwo(getPackets(str)))
 }
